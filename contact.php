871d50eab63129ddfbbc0f3c1cdc6b54840e2ec8
teecoua <m2lubomir.3d@gmail.com>
2017-10-12 18:01:11 +0300

    test

--- contact.php
+++ contact.php
@@ -47,10 +47,8 @@ else
     $responseArray = array('type' => $type, 'message' => $message);
 }
 
-if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
-    $encoded = json_encode($responseArray);
+$encoded = json_encode($responseArray);
 
-    header('Content-Type: application/json');
+header('Content-Type: application/json');
 
-    echo $encoded;
-}
+echo $encoded;
