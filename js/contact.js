3f5d2b9be29e97dc821d570894440960c837b8d6
teecoua <m2lubomir.3d@gmail.com>
2017-10-12 16:46:59 +0300

    test contact (php&js)

---
+++ js/contact.js
@@ -0,0 +1,36 @@
+$(function () {
+
+    // init the validator
+    // validator files are included in the download package
+    // otherwise download from http://1000hz.github.io/bootstrap-validator
+
+    //$('#contact-form').validator();
+
+
+    // when the form is submitted
+    $( "#telbtn" ).click(function() {
+
+        // if the validator does not prevent form submit
+        //if (!e.isDefaultPrevented()) {
+            var url = "contact.php";
+            var tel = $("#telephone").val();
+        console.log(tel);
+            // POST values in the background the the script URL
+            $.ajax({
+                type: "POST",
+                url: url,
+                data: "tel=" + tel,
+                success: function (data)
+                {
+                    console.log('success');
+                    console.log(data.type);
+                    console.log(data.message);
+                },
+                error: function(data){
+                    console.log('error');
+                }
+            });
+            return false;
+        //}
+    })
+});
---
+++ js/contact.php
@@ -0,0 +1,11 @@
+<?php
+
+$responseArray = array('type' => 'success', 'message' => 'success_message');
+
+if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+    $encoded = json_encode($responseArray);
+
+    header('Content-Type: application/json');
+
+    echo $encoded;
+}
